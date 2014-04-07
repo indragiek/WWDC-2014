50448c5e6a57bd600365274b0a7ba5a12db51979
Indragie Karunaratne <i@indragie.com>
2014-04-07 09:52:21 -0600

    Add Mantle via CocoaPods

---
+++ Indragie Karunaratne/Classes/INDMainViewItem.h
@@ -0,0 +1,13 @@
+//
+//  INDMainViewItem.h
+//  Indragie Karunaratne
+//
+//  Created by Indragie Karunaratne on 2014-04-07.
+//  Copyright (c) 2014 Indragie Karunaratne. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface INDMainViewItem : NSObject
+
+@end
---
+++ Indragie Karunaratne/Classes/INDMainViewItem.m
@@ -0,0 +1,13 @@
+//
+//  INDMainViewItem.m
+//  Indragie Karunaratne
+//
+//  Created by Indragie Karunaratne on 2014-04-07.
+//  Copyright (c) 2014 Indragie Karunaratne. All rights reserved.
+//
+
+#import "INDMainViewItem.h"
+
+@implementation INDMainViewItem
+
+@end
